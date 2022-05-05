d9850cde6e857a64f149972f28f1e2705259eabd
remus.jeberean <remus.jeberean@gmail.com>
2022-05-05 18:54:09 +0300

    App skeleton

---
+++ src/main/java/sap/ligaac/jwtproxy/JwtproxyApplication.java
@@ -0,0 +1,13 @@
+package sap.ligaac.jwtproxy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtproxyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JwtproxyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sap/ligaac/jwtproxy/JwtproxyApplicationTests.java
@@ -0,0 +1,13 @@
+package sap.ligaac.jwtproxy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtproxyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
